766b33e86718d5198e2065ce507822d20f505f9b
Daniel Santos <santos.daniel88@uabc.edu.mx>
2023-02-19 12:55:37 -0800

    comentarios innecesarios

--- P2_UART/main/p2_packagedata.c
+++ P2_UART/main/p2_packagedata.c
@@ -3,39 +3,6 @@
 #include "crc32.h"
 // pkg = (UART_Package *)malloc(N_PACKAGES * sizeof(UART_Package));
 
-// pkg[0].header = 0x5A;
-// pkg[0].command = 0x10;
-// pkg[0].length = 0;
-// pkg[0].data = (uint8_t *)malloc(PAYLOAD_LEN * sizeof(uint8_t));
-// pkg[0].end = 0xB2;
-// pkg[0].crc32 = 0;
-
-// data = (uint8_t *)malloc(BYTES_PER_PACKAGE * sizeof(uint8_t));
-
-// pkg[1].header = 0x5A;
-// pkg[1].command = 0x11;
-// pkg[1].length = 0;
-// pkg[1].data = (uint8_t *)malloc(PAYLOAD_LEN * sizeof(uint8_t));
-
-// pkg[1].end = 0xB2;
-// pkg[1].crc32 = 0;
-
-// pkg[2].header = 0x5A;
-// pkg[2].command = 0x12;
-// pkg[2].length = 0;
-// pkg[2].data = (uint8_t *)malloc(PAYLOAD_LEN * sizeof(uint8_t));
-
-// pkg[2].end = 0xB2;
-// pkg[2].crc32 = 0;
-
-// pkg[3].header = 0x5A;
-// pkg[3].command = 0x13;
-// pkg[3].length = 0;
-// pkg[3].data = (uint8_t *)malloc(PAYLOAD_LEN * sizeof(uint8_t));
-
-// pkg[3].end = 0xB2;
-// pkg[3].crc32 = 0;
-
 void createPackage(UART_Package *mi_pack, uint8_t header, uint8_t command, uint8_t length, uint8_t dat0, uint8_t dat1, uint8_t dat2, uint8_t dat3, uint8_t fin)
 {
     uint8_t input_arr[] = {header, command, length, dat0, dat1, dat2, dat3, fin};
