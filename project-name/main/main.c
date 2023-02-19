7e964f5b092ef336b94e8354463e84c9aa5863d5
Daniel Santos <santos.daniel88@uabc.edu.mx>
2023-02-18 17:30:02 -0800

    hello

---
+++ project-name/main/main.c
@@ -0,0 +1,23 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include "myUart.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+
+ 
+ 
+
+#define TXD_PIN (GPIO_NUM_17)
+#define RXD_PIN (GPIO_NUM_16)
+#define BAUD_RATE 115200
+
+#define INTERVAL 5000
+
+void app_main()
+{
+    
+}
