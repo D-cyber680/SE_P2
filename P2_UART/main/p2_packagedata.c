6432fa36a3fe7f54273634b23e1ffc71a3fe614f
Daniel Santos <santos.daniel88@uabc.edu.mx>
2023-02-19 17:27:24 -0800

    PackageToString pendiente

--- P2_UART/main/p2_packagedata.c
+++ P2_UART/main/p2_packagedata.c
@@ -16,3 +16,23 @@ void createPackage(UART_Package *mi_pack, uint8_t header, uint8_t command, uint8
     mi_pack->end = fin;
     mi_pack->crc32 = calc_crc32(input_arr, sizeof(input_arr));
 }
+
+void PackageToString(UART_Package pack, char *msg_pack)
+{
+    char buffer[MSG_TAM_STR];
+    int offset = 0;
+
+    // Convertir cada campo a una cadena y almacenarlo en el buffer temporal
+    snprintf(buffer, MSG_TAM_STR, "%02X%02X%02X", pack.header, pack.command, pack.length);
+    strcat(msg_pack, buffer);
+    offset += strlen(buffer);
+    for (int i = 0; i < PAYLOAD_LEN; i++)
+    {
+        snprintf(buffer, MSG_TAM_STR, "%02X", pack.data[i]);
+        strcat(msg_pack + offset, buffer);
+        offset += strlen(buffer);
+    }
+    snprintf(buffer, MSG_TAM_STR, "%02X%08X", pack.end, pack.crc32);
+    strcat(msg_pack + offset, buffer);
+    printf("%s", msg_pack);
+}
--- P2_UART/main/p2_packagedata.h
+++ P2_UART/main/p2_packagedata.h
@@ -3,7 +3,9 @@
 #include <inttypes.h>
 #define N_PACKAGES 4*2
 #define PAYLOAD_LEN 4
+#define BUFFER_SIZE 256
 #define BYTES_PER_PACKAGE 8
+#define MSG_TAM_STR 16
 //struct to build a package
 typedef struct UART_Package
 {
@@ -14,6 +16,7 @@ typedef struct UART_Package
 
 //returns a struct which contains package data
 void createPackage(UART_Package *mi_pack, uint8_t header, uint8_t command, uint8_t length, uint8_t dat0, uint8_t dat1, uint8_t dat2, uint8_t dat3, uint8_t fin);
+void PackageToString(UART_Package pack, char *msg_pack);
 //function that sends a package to uart
 void sendPackage(UART_Package pack);
 //function that receives a package from uart
