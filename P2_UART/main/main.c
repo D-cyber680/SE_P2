5fdc5017688da022a8e88858a875f2d7d33765df
Luis Angel Lopez Valencia <llopez17@uabc.edu.mx>
2023-02-21 15:06:22 -0800

    Mi main

--- P2_UART/main/main.c
+++ P2_UART/main/main.c
@@ -7,18 +7,120 @@
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h"
+#include "p2_packagedata.h"
+#include "crc32.h"
 
-#define MASTER 0
+#define MASTER 2
 #define SLAVE 1
 #define MODE SLAVE
 
+/*
 #define TXD_PIN (GPIO_NUM_17)
 #define RXD_PIN (GPIO_NUM_16)
+#define LED (GPIO_NUM_2)
+*/
 #define BAUD_RATE 115200
+// F comando 0x10
+uint32_t get_time_in_seconds()
+{
+    // enviar devuelta
+    char secs[20];
+    sprintf(secs, "timestamp=%d", xTaskGetTickCount() / configTICK_RATE_HZ);
+    uartPuts(0, secs);
+    uartPuts(1, secs);
+    return xTaskGetTickCount() / configTICK_RATE_HZ;
+}
 
-#define INTERVAL 5000
+// F. cmd. 0x11
+uint8_t send_led_state(uint8_t led_state)
+{
+    char led_cad[20];
+    sprintf(led_cad, "led=%d", led_state);
+    uartPuts(0, led_cad);
+    uartPuts(1, led_cad);
+    return led_state;
+}
+// F. comando 0x12
+void send_temp(void)
+{
+    // uint8_t num = rand() % 100;
+    char cad[20];
+    sprintf(cad, "temp=45");
+    uartPuts(0, cad);
+    uartPuts(1, cad);
+}
+
+// F comando 0x13
+void toggle_led_state(uint8_t *led_state)
+{
+    *led_state == 1 ? (*led_state = 0) : (*led_state = 1);
+    gpio_set_level(LED, *led_state);
+}
 
 void app_main()
 {
-    
+    uint8_t led_state = 0;
+    UART_Package *pkgs = malloc(4 * sizeof(UART_Package));
+    char msgpacks[N_PACKAGES][MSG_TAM_STR];
+
+    gpio_reset_pin(LED);
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+    //uartInit1();
+    //uartInit(0, BAUD_RATE, 8, eParityDis, eStop, UART_TX_PIN0, UART_RX_PIN0); // uart_num, baudrate,  size,  parity, stop,  txPin,  rxPin)
+    //uart_flush(1);
+    uartInit(PC_UART_PORT,115200,8,0,1,PC_UART_TX_PIN,PC_UART_RX_PIN);
+    uartInit(UART2_PORT,115200,8,0,1,UART2_TX_PIN,UART2_RX_PIN);
+    uart_flush(0);
+    uart_flush(2);
+
+    while (1)
+    {
+        // 1) Leer el paquete en forma de cadena
+        int len = uart_read_bytes(UART2_PORT, msgpacks[0], MSG_TAM_STR, pdMS_TO_TICKS(100));
+        // 2) Convertir cadena a un paquete y guardar
+        StringToPackage(&pkgs[0], msgpacks[0]);
+        // 3) Comparar crc32s:
+        if(checkCrc32(pkgs[0].crc32, msgpacks[0])==1) showPackage(pkgs[0]);
+        else uartPuts(0, "ERROR");
+        //  iguales -> fue recibido correctamente
+        //       ejecutar accion dado el comando
+        //       actualizar paquete
+        //       devolver cadena
+        //  distintos -> imprimir que no se recibio correctamente
+
+        //showPackage(pkgs[0]);
+        
+        // uartPuts(0, msgpacks[0]);
+        // uartPuts(0, "\n");
+        vTaskDelay(pdMS_TO_TICKS(3000));
+
+        //     int len = uart_read_bytes(UART_NUM_1, command, 3, pdMS_TO_TICKS(100));
+        //     if (len == 2 && command[0] == '1' && command[1] == '0')
+        //     {
+        //         uartClrScr(0);
+        //         uartPuts(0, "Comando: 0x10");
+        //         uartGotoxy(0, 5, 5);
+        //         get_time_in_seconds();
+        //     }
+        //     else if (len == 2 && command[0] == '1' && command[1] == '1')
+        //     {
+        //         uartClrScr(0);
+        //         uartPuts(0, "Comando: 0x11");
+        //         uartGotoxy(0, 5, 5);
+        //         send_led_state(led_state);
+        //     }
+        //     else if (len == 2 && command[0] == '1' && command[1] == '2')
+        //     {
+        //         uartClrScr(0);
+        //         uartPuts(0, "Comando: 0x12");
+        //         uartGotoxy(0, 5, 5);
+        //         send_temp();
+        //     }
+        //     else if (len == 2 && command[0] == '1' && command[1] == '3')
+        //     {
+        //         uartClrScr(0);
+        //         toggle_led_state(&led_state);
+        //         uartPuts(0, "Comando: 0x13");
+        //     }
+    }
 }
