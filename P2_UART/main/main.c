3f1a9949ec1b41ac6478767bd3bf9ea95c861f25
Daniel Santos <santos.daniel88@uabc.edu.mx>
2023-02-21 10:45:01 -0800

    check

--- P2_UART/main/main.c
+++ P2_UART/main/main.c
@@ -8,6 +8,7 @@
 #include "string.h"
 #include "stdlib.h"
 #include "p2_packagedata.h"
+
 #define MASTER 0
 #define SLAVE 1
 #define MODE SLAVE
@@ -23,49 +24,56 @@ void app_main()
     char secs[20];
     char led_state[20];
     char temperature[20];
+    char msgpack0[MSG_TAM_STR];
 
     uartInit1();
     uartInit(0, 115200, 8, eStop, eParityEven, UART_TX_PIN0, UART_RX_PIN0); // uart_num, baudrate,  size,  parity, stop,  txPin,  rxPin)
     uart_flush(1);
     uart_flush(0);
+
+    UART_Package *pkgs;
+    pkgs = (UART_Package *)malloc(N_PACKAGES * sizeof(UART_Package));
+
+    createPackage(&pkgs[0], 0x5A, 0x10, 0, 0, 0, 0, 0, 0xB2);
+    createPackage(&pkgs[1], 0x5A, 0x11, 0, 0, 0, 0, 0, 0xB2);
+    createPackage(&pkgs[2], 0x5A, 0x12, 0, 0, 0, 0, 0, 0xB2);
+    createPackage(&pkgs[3], 0x5A, 0x13, 0, 0, 0, 0, 0, 0xB2);
+
     while (1)
     {
-        // UART_Package *pkgs;
-        // pkgs = (UART_Package *)malloc(N_PACKAGES * sizeof(UART_Package));
-
-        // createPackage(&pkgs[0], 0x5A, 0x10, 0, 0, 0, 0, 0, 0xB2);
-        // createPackage(&pkgs[1], 0x5A, 0x11, 0, 0, 0, 0, 0, 0xB2);
-        // createPackage(&pkgs[2], 0x5A, 0x12, 0, 0, 0, 0, 0, 0xB2);
-        // createPackage(&pkgs[3], 0x5A, 0x13, 0, 0, 0, 0, 0, 0xB2);
-
-        uart_write_bytes(UART_NUM_1, "10", 2);
-        uart_read_bytes(1, secs, (READ_BUF_SIZE), 20 / portTICK_RATE_MS);
-        uartClrScr(0);
-        uartGotoxy(0, 5, 5);
-        uartPuts(0, secs);
+        PackageToString(pkgs[0], msgpack0);
+        uart_write_bytes(UART_NUM1, msgpack0, strlen(msgpack0));
+        uartPuts(0, msgpack0);
+        uartPuts(0, "\n");
         vTaskDelay(pdMS_TO_TICKS(INTERVAL));
+        // uart_write_bytes(UART_NUM_1, "10", 2);
+        // uart_read_bytes(1, secs, (READ_BUF_SIZE), 20 / portTICK_RATE_MS);
+        // uartClrScr(0);
+        // uartGotoxy(0, 5, 5);
+        // uartPuts(0, secs);
+        // vTaskDelay(pdMS_TO_TICKS(INTERVAL));
 
-        uartClrScr(0);
-        uart_write_bytes(UART_NUM_1, "11", 2);
-        uart_read_bytes(1, led_state, (READ_BUF_SIZE), 20 / portTICK_RATE_MS);
-        uartGotoxy(0, 6, 5);
-        uartPuts(0, led_state);
-        // uartPuts(0, "Sended: 0x11");
+        // uartClrScr(0);
+        // uart_write_bytes(UART_NUM_1, "11", 2);
+        // uart_read_bytes(1, led_state, (READ_BUF_SIZE), 20 / portTICK_RATE_MS);
+        // uartGotoxy(0, 6, 5);
+        // uartPuts(0, led_state);
+        // // uartPuts(0, "Sended: 0x11");
 
-        vTaskDelay(pdMS_TO_TICKS(INTERVAL));
+        // vTaskDelay(pdMS_TO_TICKS(INTERVAL));
 
-        uartClrScr(0);
-        uart_write_bytes(UART_NUM_1, "12", 2);
-        uart_read_bytes(1, temperature, (READ_BUF_SIZE), 20 / portTICK_RATE_MS);
-        uartGotoxy(0, 7, 5);
-        uartPuts(0, temperature);
-        // uartPuts(0, "Sended: 0x12");
-        vTaskDelay(pdMS_TO_TICKS(INTERVAL));
+        // uartClrScr(0);
+        // uart_write_bytes(UART_NUM_1, "12", 2);
+        // uart_read_bytes(1, temperature, (READ_BUF_SIZE), 20 / portTICK_RATE_MS);
+        // uartGotoxy(0, 7, 5);
+        // uartPuts(0, temperature);
+        // // uartPuts(0, "Sended: 0x12");
+        // vTaskDelay(pdMS_TO_TICKS(INTERVAL));
 
-        uart_write_bytes(UART_NUM_1, "13", 2);
-        uartClrScr(0);
-        uartGotoxy(0, 8, 5);
-        uartPuts(0, "0x13: Invertimos LED");
-        vTaskDelay(pdMS_TO_TICKS(INTERVAL));
+        // uart_write_bytes(UART_NUM_1, "13", 2);
+        // uartClrScr(0);
+        // uartGotoxy(0, 8, 5);
+        // uartPuts(0, "0x13: Invertimos LED");
+        // vTaskDelay(pdMS_TO_TICKS(INTERVAL));
     }
 }
