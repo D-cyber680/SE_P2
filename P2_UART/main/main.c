80820669c39faa504757a1a85578f76b3c3c9e93
Daniel Santos <santos.daniel88@uabc.edu.mx>
2023-02-21 19:15:49 -0800

    HASTA AQUI

--- P2_UART/main/main.c
+++ P2_UART/main/main.c
@@ -25,14 +25,16 @@ void app_main()
     char secs[20];
     char led_state[20];
     char temperature[20];
-    char msgpacks[4][MSG_TAM_STR];
+    char msgpacks[MSG_TAM_STR];
     char feedBackPack[24];
     volatile uint8_t offsetX = 0;
     volatile uint8_t offsetY = 0;
+    
     uartInit1();
     uartInit(0, 115200, 8, eStop, eParityEven, UART_TX_PIN0, UART_RX_PIN0); // uart_num, baudrate,  size,  parity, stop,  txPin,  rxPin)
     uart_flush(1);
     uart_flush(0);
+    uartClrScr(0);
 
     UART_Package *pkgs;
     pkgs = (UART_Package *)malloc(4 * sizeof(UART_Package));
@@ -44,46 +46,17 @@ void app_main()
 
     while (1)
     {
-        PackageToString(pkgs[i], msgpacks[i]);
-        uart_write_bytes(UART_NUM1, msgpacks[i], strlen(msgpacks[i]));
+        PackageToString(pkgs[i], msgpacks);
+        uart_write_bytes(UART_NUM1, msgpacks, strlen(msgpacks));
         uart_read_bytes(UART_NUM1, feedBackPack, 24, pdMS_TO_TICKS(100));
         uartPuts(0, feedBackPack);
         uartGotoxy(0, offsetX, offsetY);
 
         vTaskDelay(pdMS_TO_TICKS(INTERVAL));
 
-        offsetX += 10;
         offsetY += 10;
         i++;
         i = i % 4;
-        // uart_write_bytes(UART_NUM_1, "10", 2);
-        // uart_read_bytes(1, secs, (READ_BUF_SIZE), 20 / portTICK_RATE_MS);
-        // uartClrScr(0);
-        // uartGotoxy(0, 5, 5);
-        // uartPuts(0, secs);
-        // vTaskDelay(pdMS_TO_TICKS(INTERVAL));
 
-        // uartClrScr(0);
-        // uart_write_bytes(UART_NUM_1, "11", 2);
-        // uart_read_bytes(1, led_state, (READ_BUF_SIZE), 20 / portTICK_RATE_MS);
-        // uartGotoxy(0, 6, 5);
-        // uartPuts(0, led_state);
-        // // uartPuts(0, "Sended: 0x11");
-
-        // vTaskDelay(pdMS_TO_TICKS(INTERVAL));
-
-        // uartClrScr(0);
-        // uart_write_bytes(UART_NUM_1, "12", 2);
-        // uart_read_bytes(1, temperature, (READ_BUF_SIZE), 20 / portTICK_RATE_MS);
-        // uartGotoxy(0, 7, 5);
-        // uartPuts(0, temperature);
-        // // uartPuts(0, "Sended: 0x12");
-        // vTaskDelay(pdMS_TO_TICKS(INTERVAL));
-
-        // uart_write_bytes(UART_NUM_1, "13", 2);
-        // uartClrScr(0);
-        // uartGotoxy(0, 8, 5);
-        // uartPuts(0, "0x13: Invertimos LED");
-        // vTaskDelay(pdMS_TO_TICKS(INTERVAL));
     }
 }
